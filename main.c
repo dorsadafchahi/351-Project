8dea0c1e1bf8861b27237b32cbb1bc41f0ac7c2f
hunger2K <nicholashung7@gmail.com>
2023-12-03 15:30:19 -0800

    Main file, nothing in it

---
+++ main.c
@@ -0,0 +1,16 @@
+//this is the final project for class ENSC 351
+//
+//  Nicholas H, Dorsa A, Lester P, Jim P
+//
+//  Description: Water pollution detection
+//  Will use 2 turbidity sensors to detect the amount of pollution in water, and then display that amount on an LED
+//  will use modules, threads and Gpio/I2c access to the beaglebone
+#include <stdio.h>
+//#include "LEDmatrix.h"
+
+
+int main(){
+
+
+
+}
